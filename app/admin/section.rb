e54cb9415326d853906b9c055b76fd22cd8cd295
Prelang Builder <builder@prelang.com>
2016-08-31 06:42:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Section' to allow for create, update

--- app/admin/section.rb
+++ app/admin/section.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Section do
+  permit_params :title, :description, :image, :slug, :visible
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
