5eb6935e2a2364ab368d7bf6d40345817a972f06
Prelang Builder <builder@prelang.com>
2016-08-31 06:41:46 +0000

    Defined validation for attribute 'image' on model 'Group'

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :users
   has_and_belongs_to_many :characters
+  validates_formatting_of :image, using: :url
 end
