56143d874f2baadd0329c5e8d7d986c4de231a8b
Prelang Builder <builder@prelang.com>
2016-08-31 06:41:46 +0000

    Defined validation for attribute 'poster_ip' on model 'Post'

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   belongs_to :story
   has_and_belongs_to_many :characters
   validates_formatting_of :image, using: :url
+  validates_formatting_of :poster_ip, using: :ip_address_v4
 end
