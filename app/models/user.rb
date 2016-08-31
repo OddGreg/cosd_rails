b2d069f831d4290ba20928b4eb74905a04b59e0b
Prelang Builder <builder@prelang.com>
2016-08-31 06:41:46 +0000

    Installed relation 'has_and_belongs_to_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   has_many :characters
   has_many :stories
   has_many :posts
+  has_and_belongs_to_many :groups
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
