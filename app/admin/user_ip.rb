0b1a1fef28793be8897ff63779d56bf90fc1894b
Prelang Builder <builder@prelang.com>
2016-08-31 06:41:49 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'UserIp' to allow for create, update

--- app/admin/user_ip.rb
+++ app/admin/user_ip.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register UserIp do
+  permit_params :ip_address
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
